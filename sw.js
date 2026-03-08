0aac74215ae7d0f723d35a0dff9f1eaef24a79d6
SisGRO-TCOPM <gustavo_tgagro@hotmail.com>
2026-03-08 16:50:02 -0300

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "mapa-rural-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "https://unpkg.com/leaflet/dist/leaflet.css",
+  "https://unpkg.com/leaflet/dist/leaflet.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
